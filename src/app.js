182d275f410345de7d0e917c0ad24d1d80a81c5d
Hoozia <meheny@naver.com>
2023-04-13 19:14:58 +0900

    file 생성 추가 - TODO fine-tune

--- src/app.js
+++ src/app.js
@@ -1,27 +1,34 @@
 import express from 'express';
 import path from 'path';
 import { handleInput } from './open-ai.js';
-import bodyParser from 'body-parser';
+import { fileTest } from './open-ai-file.js';
 
 
 const app = express();
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-app.use(bodyParser.json());
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/response.html');
 });
 
-app.post('/', async (req, res) => {
+
+app.get('/file-test', (req, res) => {
+  fileTest()
+
+  res.send('file test');
+});
+
+app.post('/', async (req, res, next) => {
+  const conversation_history = req.body;
   try {
-    const conversation_history = req.body;
     const html = await handleInput(conversation_history);
     res.send(html);
   } catch (error) {
-    res.status(500).send(error.message);
+    next(error);
   }
 });
 
--- src/open-ai.js
+++ src/open-ai.js
@@ -1,10 +1,16 @@
+import * as fs from "fs";
 import { Configuration, OpenAIApi } from "openai";
 import * as marked from "marked";
 import { config } from "dotenv";
 import { get_encoding, encoding_for_model } from "@dqbd/tiktoken";
+import path from "path";
 
+config(); // dotenv 설정
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const filePath = __dirname + "/open-ai.js";
+const fine_tunes = [];
 const model = "gpt-3.5-turbo";
-
 
 config(); // dotenv 설정
 
@@ -57,7 +63,37 @@ export async function handleInput(conversation_history) {
         "You are a nodejs, html developer assistant and code reviewer. And answer me in English",
     },
   ];
-  
+  // 데이터를 파일로 저장합니다.
+  if (fine_tunes && fine_tunes.length === 0) {
+    const filePath = __dirname + "/fine_tunes.json";
+
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+      console.log("기존 파일 삭제 완료");
+    }
+    // 주어진 데이터를 가공하여 결과 배열에 추가
+    for (let i = 0; i < conversation_history.length; i += 2) {
+      // assistant가 빠져있을 경우, user content도 제거
+      if (
+        conversation_history[i + 1] === undefined ||
+        conversation_history[i + 1].role !== "assistant"
+      ) {
+        continue;
+      }
+      const prompt = conversation_history[i].content;
+      const completion = conversation_history[i + 1].content;
+      fine_tunes.push({ prompt: prompt, completion: completion });
+    }
+
+    const stream = fs.createWriteStream(filePath, { flags: "a" });
+    for (let i = 0; i < fine_tunes.length; i++) {
+      const jsonStr = JSON.stringify(fine_tunes[i]);
+      stream.write(jsonStr + "\n");
+    }
+    stream.end();
+    console.log("fine_tunes 파일 쓰기 완료");
+  }
+
   let currentUseToken = 0;
   for (let i = conversation_history.length - 1; i >= 0; i--) {
     currentUseToken += enc.encode(conversation_history[i].content).length;
