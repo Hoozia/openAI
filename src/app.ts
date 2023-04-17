3bdb8fe696339dbd449247b40d8cc54a5813bce5
Hoozia <meheny@naver.com>
2023-04-18 00:59:29 +0900

    llama version up

--- src/app.ts
+++ src/app.ts
@@ -7,57 +7,56 @@ import { createImageAI } from "./open-ai-image.js";
 import { LLama } from "llama-node";
 import { LLamaCpp, LoadConfig } from "llama-node/dist/llm/llama-cpp.js";
 import { LLamaRS } from "llama-node/dist/llm/llama-rs.js";
-import fs  from "fs";
+import fs from "fs";
 
 const app = express();
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 //TODO : https://github.com/hlhr202/llama-node#usage-llamacpp-backend 참고해서 구현해보기
 //TODO : https://www.youtube.com/watch?v=vNHjeQxNuS0 이것도 참고
-// const model = path.resolve(process.cwd(), "./src/ggml-vicuna-13b-1.1-q4_0.bin");
+const model = path.resolve(process.cwd(), "./src/ggml-model-q4_1.bin");
 
+const llama = new LLama(LLamaCpp);
 
-// const llama = new LLama(LLamaCpp);
+const config: LoadConfig = {
+  path: model,
+  enableLogging: true,
+  nCtx: 1024,
+  nParts: -1,
+  seed: 0,
+  f16Kv: false,
+  logitsAll: false,
+  vocabOnly: false,
+  useMlock: false,
+  embedding: false,
+  useMmap: true,
+};
 
-// const config: LoadConfig = {
-//     path: model,
-//     enableLogging: true,
-//     nCtx: 1024,
-//     nParts: -1,
-//     seed: 0,
-//     f16Kv: false,
-//     logitsAll: false,
-//     vocabOnly: false,
-//     useMlock: false,
-//     embedding: false,
-// };
+llama.load(config);
 
-// llama.load(config);
+const template = `node js implementation of express server`;
 
-// const template = `How are you`;
+const prompt = `### Human:
 
-// const prompt = `### Human:
+${template}
 
-// ${template}
+### Assistant:`;
 
-// ### Assistant:`;
-
-// llama.createCompletion(
-//     {
-//         nThreads: 4,
-//         nTokPredict: 2048,
-//         topK: 40,
-//         topP: 0.1,
-//         temp: 0.2,
-//         repeatPenalty: 1,
-//         stopSequence: "### Human",
-//         prompt,
-//     },
-//     (response) => {
-//         process.stdout.write(response.token);
-//     }
-// );
-
+llama.createCompletion(
+  {
+    nThreads: 4,
+    nTokPredict: 2048,
+    topK: 40,
+    topP: 0.1,
+    temp: 0.7,
+    repeatPenalty: 1,
+    stopSequence: "### Human",
+    prompt,
+  },
+  (response) => {
+    process.stdout.write(response.token);
+  }
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -84,20 +83,17 @@ app.post("/", async (req, res, next) => {
 //   res.send("file test");
 // });
 
-
 app.get("/create-image", async (req, res) => {
-  res.sendFile(__dirname + "/image.html");
+  res.sendFile(path.resolve(process.cwd(), "./src/image.html"));
 });
 
 app.post("/create-image", async (req, res) => {
   const body = req.body;
-  const requestImage = body.request_image
+  const requestImage = body.request_image;
   const imageUrl = await createImageAI(requestImage);
 
   return res.send(imageUrl);
 });
-
-
 
 app.listen(15000, () => {
   console.log(`
