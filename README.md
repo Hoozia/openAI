56415de0b25a51ae54c2a6b401e633ad069e2490
Hoozia <meheny@naver.com>
2023-04-18 01:08:43 +0900

    binary 없으면 에러 발생할 수 있어 예외 처리 및 README 작성

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ localhost:15000
 기본 질문 형태와 답변형태의 페이지가 출력됨
 
 아래에 질문 입력 후 엔터 및 메시지 전송 버튼 클릭시 응답
+
+
+## 에러 LOG 발생 예측
+app.ts의 17 ~ 59line llama 사용하는 부분에서 콘솔 에러 로그 발생
+해당 부분은 로컬 ai 모델을 기동하는 부분으로 binary가 필요하기 때문에 없다면 에러 발생
+binary는 https://huggingface.co/Pi3141/alpaca-lora-30B-ggml 에서 다운로드해서 사용 중
--- src/app.ts
+++ src/app.ts
@@ -11,52 +11,56 @@ import fs from "fs";
 
 const app = express();
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
+try {
+  //TODO : https://github.com/hlhr202/llama-node#usage-llamacpp-backend 참고해서 구현해보기
+  //TODO : https://www.youtube.com/watch?v=vNHjeQxNuS0 이것도 참고
+  const model = path.resolve(process.cwd(), "./src/ggml-model-q4_1.bin");
 
-//TODO : https://github.com/hlhr202/llama-node#usage-llamacpp-backend 참고해서 구현해보기
-//TODO : https://www.youtube.com/watch?v=vNHjeQxNuS0 이것도 참고
-const model = path.resolve(process.cwd(), "./src/ggml-model-q4_1.bin");
+  const llama = new LLama(LLamaCpp);
 
-const llama = new LLama(LLamaCpp);
+  const config: LoadConfig = {
+    path: model,
+    enableLogging: true,
+    nCtx: 1024,
+    nParts: -1,
+    seed: 0,
+    f16Kv: false,
+    logitsAll: false,
+    vocabOnly: false,
+    useMlock: false,
+    embedding: false,
+    useMmap: true,
+  };
 
-const config: LoadConfig = {
-  path: model,
-  enableLogging: true,
-  nCtx: 1024,
-  nParts: -1,
-  seed: 0,
-  f16Kv: false,
-  logitsAll: false,
-  vocabOnly: false,
-  useMlock: false,
-  embedding: false,
-  useMmap: true,
-};
+  llama.load(config);
 
-llama.load(config);
+  const template = `node js implementation of express server`;
 
-const template = `node js implementation of express server`;
-
-const prompt = `### Human:
+  const prompt = `### Human:
 
 ${template}
 
 ### Assistant:`;
 
-llama.createCompletion(
-  {
-    nThreads: 4,
-    nTokPredict: 2048,
-    topK: 40,
-    topP: 0.1,
-    temp: 0.7,
-    repeatPenalty: 1,
-    stopSequence: "### Human",
-    prompt,
-  },
-  (response) => {
-    process.stdout.write(response.token);
-  }
-);
+  llama.createCompletion(
+    {
+      nThreads: 4,
+      nTokPredict: 2048,
+      topK: 40,
+      topP: 0.1,
+      temp: 0.7,
+      repeatPenalty: 1,
+      stopSequence: "### Human",
+      prompt,
+    },
+    (response) => {
+      process.stdout.write(response.token);
+    }
+  );
+} catch (err) {
+  console.error("로컬 AI 기동 중 에러 발생");
+  console.error("err : ", err);
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
